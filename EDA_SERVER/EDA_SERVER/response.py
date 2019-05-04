41b11303d6081486e2a7df4302aab4fb51fefe47
libaoling <17752510019@163.com>
2019-05-04 20:02:40 +0800

    第一次提交

---
+++ EDA_SERVER/EDA_SERVER/response.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+
+def r(success=True, result=''):
+    _r = dict()
+    if success is True:
+        _r['status_code'] = 200
+    else:
+        _r['status_code'] = 400
+    _r['result'] = result
+
+    return _r
+
